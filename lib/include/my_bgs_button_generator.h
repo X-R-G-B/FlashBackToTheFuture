f809a7b448c1683e861334274bec891f68637bcc
guillaumeAbel <guillaume.abel@epitech.eu>
2022-03-24 17:01:11 +0100

    Add functionnal event setting

---
+++ lib/include/my_bgs_button_generator.h
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2022
+** my bgs
+** File description:
+** button generator
+*/
+
+#ifndef BUTTON_H_
+    #define BUTTON_H_
+
+    #include "my_bgs_components.h"
+    #include "my_json.h"
+
+void click_scene_2(object_t *, scene_t *, window_t *, set_event_t *);
+void off_click_scene_2(object_t *, scene_t *, window_t *, set_event_t *);
+
+static void (*on_click[])(object_t *, scene_t *, window_t *,
+    set_event_t *) = {NULL, click_scene_2};
+
+static const char *str_on_click[] = {"scene 2", NULL};
+
+static void (*off_click[])(object_t *, scene_t *, window_t *,
+    set_event_t *) = {NULL, off_click_scene_2};
+
+static const char *str_off_click[] = {NULL};
+
+static void (*on_hover[])(object_t *, scene_t *, window_t *,
+    set_event_t *) = {NULL};
+
+static const char *str_on_hover[] = {NULL};
+
+static void (*off_hover[])(object_t *, scene_t *, window_t *,
+    set_event_t *) = {NULL};
+
+static const char *str_off_hover[] = {NULL};
+
+int set_event(object_t *object, dico_t *dico);
+
+list_ptr_t *fill_obj_list(list_t *elem, scene_t *scene);
+
+int set_clr(object_t *object, dico_t *dico);
+
+#endif /* !BUTTON_H_ */
