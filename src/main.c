b31662bdb7559c7d960d799dcb9af896ddf8df79
saverio <xavier.mitault@epitech.eu>
2022-03-28 17:13:01 +0200

    Fix warnings when compiled main.c

--- src/main.c
+++ src/main.c
@@ -10,19 +10,27 @@
 
 const char *str_on_hover[] = {NULL};
 
-void (*on_hover[])(object_t *, scene_t *, window_t *win, set_event_t *);
+void (*on_hover[])(object_t *, scene_t *, window_t *win, set_event_t *) = {
+    NULL
+};
 
 const char *str_off_hover[] = {NULL};
 
-void (*off_hover[])(object_t *, scene_t *, window_t *win, set_event_t *);
+void (*off_hover[])(object_t *, scene_t *, window_t *win, set_event_t *) = {
+    NULL
+};
 
 const char *str_on_click[] = {NULL};
 
-void (*on_click[])(object_t *, scene_t *, window_t *win, set_event_t *);
+void (*on_click[])(object_t *, scene_t *, window_t *win, set_event_t *) = {
+    NULL
+};
 
 const char *str_off_click[] = {NULL};
 
-void (*off_click[])(object_t *, scene_t *, window_t *win, set_event_t *);
+void (*off_click[])(object_t *, scene_t *, window_t *win, set_event_t *) = {
+    NULL
+};
 
 int main(void)
 {
