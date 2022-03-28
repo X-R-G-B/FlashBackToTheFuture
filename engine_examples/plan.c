1f3872f79197540ff926b75c46d0fb21270a8125
Xavier Mitault <xavier.mitault@epitech.eu>
2022-03-28 16:37:43 +0200

    Revert "Fix examples"
    This reverts commit 2fc93611a48e66e56cad6eac14d27086a95277f6.

--- engine_examples/plan.c
+++ engine_examples/plan.c
@@ -12,7 +12,7 @@ int main(void)
 {
     sfVideoMode mode = {1920, 1080, 32};
     window_t *win = create_window(mode, "t", sfResize | sfClose);
-    scene_t *scene = create_scene(win, sfBlack, "SALUT");
+    scene_t *scene = create_scene(win, sfBlack);
 
     object_set_sprite(create_object(NULL, NULL, scene, 1), "./tests/path.png",
         (sfIntRect) {-1, -1, -1, -1}, (sfVector2f) {405, 405});
@@ -24,7 +24,6 @@ int main(void)
         (sfIntRect) {-1, -1, -1, -1}, (sfVector2f) {430, 430});
     object_set_sprite(create_object(NULL, NULL, scene, 2), "./tests/dirt.png",
         (sfIntRect) {-1, -1, -1, -1}, (sfVector2f) {410, 410});
-    window_change_scene(win, "SALUT");
     loop(win);
     remove_window(win);
     return (0);
