b7e23f69929df5ac39270265fbcb57e7da0c8a73
guillaumeAbel <guillaume.abel@epitech.eu>
2022-03-29 10:28:26 +0200

    FBBTF-18 Fix norm error

--- engine_examples/ex.c
+++ engine_examples/ex.c
@@ -7,16 +7,6 @@
 
 #include "my_bgs.h"
 
-void hov(object_t *obj, scene_t *scene, window_t *win, set_event_t *evt)
-{
-    sfSprite_setScale(obj->drawable.sprite, (sfVector2f) {1.05, 1.05});
-}
-
-void hove(object_t *obj, scene_t *scene, window_t *win, set_event_t *evt)
-{
-    sfSprite_setScale(obj->drawable.sprite, (sfVector2f) {1.0, 1.0});
-}
-
 void offf(object_t *obj, scene_t *scene, window_t *win, set_event_t *evt)
 {
     my_putstr(1, "fin a\n");
@@ -39,13 +29,11 @@ void scdclick(object_t *obj, scene_t *scene, window_t *win, set_event_t *evt)
 
 const char *str_on_hover[] = {"fst", "scd", NULL};
 
-void (*on_hover[])(object_t *, scene_t *, window_t *, set_event_t *) =
-    {hov, hov};
+void (*on_hover[])(object_t *, scene_t *, window_t *, set_event_t *);
 
 const char *str_off_hover[] = {"fst", "scd", NULL};
 
-void (*off_hover[])(object_t *, scene_t *, window_t *, set_event_t *) =
-    {hove, hove};
+void (*off_hover[])(object_t *, scene_t *, window_t *, set_event_t *);
 
 const char *str_on_click[] = {"fst", "scd", NULL};
 
