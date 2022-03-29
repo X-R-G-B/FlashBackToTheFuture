8550eea2f3449dfb982306603e18abdb768d7505
Romain <romain.panno@epitech.eu>
2022-03-29 20:34:43 +0200

    Add set_sprite_rect func in order to changer rect of a sprite and change origin for good pos Add begin of test for player's movements

--- include/my_rpg.h
+++ include/my_rpg.h
@@ -6,9 +6,13 @@
 */
 
 #ifndef RPG_H_
-#define RPG_H_
+    #define RPG_H_
 
-#include "my_bgs.h"
+    #include "my_bgs.h"
+
+    #define RET_OK 0
+    #define RET_ERR_MALLOC 1
+    #define RET_ERR_INPUT 2
 
 typedef enum state_e {
     STOP,
