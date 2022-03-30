6377372ed8ccc719e6f546337018cde2befbbc95
Romain <romain.panno@epitech.eu>
2022-03-30 19:18:30 +0200

    Fix my_atof : better header

--- lib/libconv/src/my_atof.c
+++ lib/libconv/src/my_atof.c
@@ -1,8 +1,8 @@
 /*
 ** EPITECH PROJECT, 2022
-** B-MUL-200-TLS-2-1-mydefender-baptiste.laran
-** File description:
 ** my_atof
+** File description:
+** change string to float (double)
 */
 
 #include <stddef.h>
