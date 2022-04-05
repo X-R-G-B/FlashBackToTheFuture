d0bf1e81590bbd1d33292f45a0aed27f5efdb7b8
saverio <xavier.mitault@epitech.eu>
2022-04-05 22:07:54 +0200

    Fill buffer with distance with the start to be used by the ennemy

--- include/ennemy_pathfind.h
+++ include/ennemy_pathfind.h
@@ -8,7 +8,15 @@
 #ifndef ENNEMY_PATHFIND_H_
     #define ENNEMY_PATHFIND_H_
 
+    #include "list.h"
+
 typedef struct pathfind_impl_s pathfind_impl_t;
+typedef struct vect2i_s vect2i_t;
+
+struct vect2i_s {
+    int x;
+    int y;
+};
 
 struct pathfind_impl_s {
     char **origins;
@@ -17,6 +25,12 @@ struct pathfind_impl_s {
     int size_y;
     char end_char;
     char start_char;
+    vect2i_t end;
+    vect2i_t start;
 };
+
+int put_distance_buffer(pathfind_impl_t *maps);
+
+int check_start_in_list(list_ptr_t *list, vect2i_t *start);
 
 #endif
