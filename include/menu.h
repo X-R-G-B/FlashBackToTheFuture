70e496b9c7b071bf6760a5bfea260f67d5284ff3
KitetsuK <brice.desousa@epitech.eu>
2022-04-05 19:17:46 +0200

    Modif include/menu.h remove toggle pop up

--- include/menu.h
+++ include/menu.h
@@ -15,7 +15,6 @@
 
 void set_is_visible_false(list_ptr_t *);
 int init_menu(window_t *);
-void toggle_pop_up(dico_t *dico, char *key);
 void close_window(object_t *, scene_t *, window_t *, set_event_t *);
 void free_pop_up(void *list);
 int check_if_pop_up_true(dico_t *dico, char *key);
