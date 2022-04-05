c77cc605c9f2efa353ecc4f445d751693989b868
saverio <xavier.mitault@epitech.eu>
2022-04-05 23:29:14 +0200

    Add return of init_find be more utils if the return is added to a components of the ennemy, it will be usable by another function of the ennemy_pathfind ecosystem

--- src/pathfind/init_find.c
+++ src/pathfind/init_find.c
@@ -5,7 +5,7 @@
 ** init the find path
 */
 
-#include <stddef.h>
+#include <stdlib.h>
 #include "list.h"
 #include "my_wordarray.h"
 #include "my_strings.h"
@@ -48,23 +48,55 @@ static int fill_vect_pos(pathfind_impl_t *maps)
     return (-1);
 }
 
-int init_pathfind(char **array, char end, char start)
+static pathfind_t *create_pathfind(pathfind_impl_t *maps)
 {
-    pathfind_impl_t maps = {
-        .origins = array,
-        .start_char = start,
-        .end_char = end
-    };
+    pathfind_t *path = NULL;
 
-    maps.size_y = my_wordarray_len(array);
-    if (maps.size_y == 0 || end == '\0' || start == '\0') {
+    path = malloc(sizeof(pathfind_t));
+    if (path == NULL) {
+        return (NULL);
+    }
+    path->end = maps->end;
+    path->map = maps->buffer;
+    path->sizex = maps->size_x;
+    path->sizey = maps->size_y;
+    return (path);
+}
+
+static int fill_pathfind_impl(pathfind_impl_t *maps, char **array, char end,
+        char start)
+{
+    if (array == NULL) {
         return (-1);
     }
-    maps.size_x = my_strlen(array[0]);
-    maps.buffer = my_wordarray_from_wordarray(array);
-    if (maps.buffer == NULL || init_new_buffer(&maps) || fill_vect_pos(&maps)) {
+    maps->origins = array;
+    maps->start_char = start;
+    maps->end_char = end;
+    maps->size_y = my_wordarray_len(array);
+    if (maps->size_y == 0 || end == '\0' || start == '\0') {
         return (-1);
     }
-    put_distance_buffer(&maps);
+    maps->size_x = my_strlen(array[0]);
+    maps->buffer = my_wordarray_from_wordarray(array);
+    if (maps->buffer == NULL) {
+        return (-1);
+    }
     return (0);
+}
+
+pathfind_t *init_pathfind(char **array, char end, char start)
+{
+    pathfind_impl_t maps = {0};
+    pathfind_t *path = NULL;
+
+    if (fill_pathfind_impl(&maps, array, end, start)) {
+        return (NULL);
+    }
+    if (init_new_buffer(&maps) || fill_vect_pos(&maps) ||
+            put_distance_buffer(&maps)) {
+        return (NULL);
+    }
+    path = create_pathfind(&maps);
+    my_wordarray_free(maps.buffer);
+    return (path);
 }
