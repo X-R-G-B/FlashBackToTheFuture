1310a3535339da87d178de0cc88f42c6471fda45
guillaumeAbel <guillaume.abel@epitech.eu>
2022-04-07 12:37:12 +0200

    Add create collision map from real map collision data

--- Makefile
+++ Makefile
@@ -29,7 +29,8 @@ SRC_LAUNCH	:=	launch_game.c	\
 				launch_story_mode.c
 SRC_LAUNCH	:= $(addprefix launch/,$(SRC_LAUNCH))
 
-SRC_MAP		:=	stage_map_to_collision_array.c
+SRC_MAP		:=	stage_map_to_collision_array.c	\
+				create_collision_map.c
 SRC_MAP		:= $(addprefix map/,$(SRC_MAP))
 
 SRC_EVENT	:=	attack.c	\
--- src/map/stage_map_to_collision_array.c
+++ src/map/stage_map_to_collision_array.c
@@ -7,7 +7,57 @@
 
 #include "my_rpg.h"
 #include "my_wordarray.h"
+#include "my_strings.h"
 #include "my_json.h"
+
+static char check_char_type(any_t *char_dict)
+{
+    any_t *collision = dico_t_get_any(char_dict->value.dict, "col");
+
+    if (collision == NULL || collision->type != INT ||
+        collision->value.i == 0) {
+        return '.';
+    } else {
+        return '#';
+    }
+}
+
+static int fill_current_string(char **map, int i, any_t *char_type, char **new)
+{
+    char current_char[2] = {'\0', '\0'};
+    int str_len = my_strlen(map[i]);
+    any_t *char_dict = NULL;
+
+    for (int x = 0; x < str_len; x++) {
+        current_char[0] = map[i][x];
+        char_dict = dico_t_get_any(char_type->value.dict, current_char);
+        if (char_dict == NULL && map[i][x] != ' ') {
+            return RET_ERR_INPUT;
+        } else if (map[i][x] == ' ') {
+            new[i][x] = '#';
+        } else {
+            new[i][x] = check_char_type(char_dict);
+        }
+    }
+    return RET_OK;
+}
+
+static char **get_right_char(char **map, any_t *char_type)
+{
+    int arr_len = my_wordarray_len(map);
+    char **new = create_new_map(map);
+
+    if (char_type == NULL || char_type->type != DICT || new == NULL) {
+        if (new != NULL) {
+            my_wordarray_free(new);
+        }
+        return NULL;
+    }
+    for (int i = 0; i < arr_len; i++) {
+        fill_current_string(map, i, char_type, new);
+    }
+    return new;
+}
 
 char **stage_map_to_collision_array(scene_t *scene)
 {
@@ -19,9 +69,6 @@ char **stage_map_to_collision_array(scene_t *scene)
         return NULL;
     }
     stage_data = dico_t_get_any(scene->components, SAVE);
-    if (stage_data == NULL) {
-        return NULL;
-    }
     map = get_from_any(stage_data, "dd", "map data", "map");
     if (map == NULL || map->type != ARRAY) {
         return NULL;
@@ -30,5 +77,6 @@ char **stage_map_to_collision_array(scene_t *scene)
     if (stage_map == NULL) {
         return NULL;
     }
-    return stage_map;
+    return get_right_char(stage_map, get_from_any(stage_data, "dd", "map data",
+        "char type"));
 }
