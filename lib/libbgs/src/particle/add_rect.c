38947bde766392a3020e5ca714dc3fc363a048b8
saverio <xavier.mitault@epitech.eu>
2022-04-08 21:23:18 +0200

    Add create line adn rect

---
+++ lib/libbgs/src/particle/add_rect.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2022
+** LIBBGS
+** File description:
+** add a rect to framebuffer
+*/
+
+#include <stdlib.h>
+#include "my_bgs.h"
+#include "my_bgs_framebuffer.h"
+#include "framebuffer.h"
+
+static int init_values(elem_rect_t *dest, elem_rect_t *src)
+{
+    if (dest == NULL || src == NULL) {
+        return (1);
+    }
+    dest->is_plain = src->is_plain;
+    dest->rect = src->rect;
+    return (0);
+}
+
+int fb_add_rect(framebuffer_t *buf, elem_rect_t rect_info,
+    void (*update)(void *element, framebuffer_t *buf))
+{
+    struct element_s *new = NULL;
+
+    if (buf == NULL) {
+        return (BGS_ERR_INPUT);
+    }
+    new = add_element(buf, RECT, update);
+    if (new == NULL) {
+        return (BGS_ERR_MALLOC);
+    }
+    if (init_values(&new->data.rect, &rect_info) == 1) {
+        return (BGS_ERR_INPUT);
+    }
+    return (BGS_OK);
+}
