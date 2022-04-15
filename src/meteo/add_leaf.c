4f8f7c9b071ac1eb1f20279a0853ed387fe8bb49
saverio <xavier.mitault@epitech.eu>
2022-04-15 14:04:04 +0200

    Fix color to fit leaf

--- src/meteo/add_leaf.c
+++ src/meteo/add_leaf.c
@@ -34,20 +34,20 @@ int add_leaf(window_t *win, int x)
     int sizey = (rand() % 2 == 0) ? 10 : 20;
     elem_rect_t rect = {
         .rect = (sfFloatRect) {x, 0, sizex, sizey},
-        .color = sfColor_fromRGBA(255, 0, 0, 255),
+        .color = sfColor_fromRGBA(255, 0, 0, 150 + rand() % 100),
         .is_plain = true
     };
     elem_rect_t rect2 = {
         .rect = (sfFloatRect) {x + (int) (sizex / 2) - 2, sizey / 2.0,
             2, sizey},
-        .color = sfColor_fromRGBA(255, 0, 0, 255),
+        .color = sfColor_fromRGBA(0, 0, 0, 200 + rand() % 50),
         .is_plain = true
     };
 
     if (win == NULL) {
         return (RET_ERR_INPUT);
     }
-    fb_add_rect(win->buf, rect, update_leaf);
     fb_add_rect(win->buf, rect2, update_leaf);
+    fb_add_rect(win->buf, rect, update_leaf);
     return (RET_OK);
 }
