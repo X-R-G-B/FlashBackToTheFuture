d258abf67f26a88b4f9374a62b0a30a5db52b04f
KitetsuK <brice.desousa@epitech.eu>
2022-04-19 17:54:57 +0200

    ADD new assets MODIF check if scene == NULL

--- src/menu/main/on_click_event.c
+++ src/menu/main/on_click_event.c
@@ -13,7 +13,7 @@ void click_play(object_t *obj, scene_t *scene, window_t *win,
     list_ptr_t *buttons = dico_t_get_value(scene->components, MENU);
     object_t *play = NULL;
 
-    if (buttons == NULL) {
+    if (buttons == NULL || scene == NULL) {
         return;
     }
     play = get_element_i_var(buttons, 1);
@@ -26,7 +26,7 @@ void click_settings(object_t *obj, scene_t *scene, window_t *win,
     list_ptr_t *buttons = dico_t_get_value(scene->components, MENU);
     object_t *settings = NULL;
 
-    if (buttons == NULL) {
+    if (buttons == NULL || scene == NULL) {
         return;
     }
     settings = get_element_i_var(buttons, 2);
@@ -39,7 +39,7 @@ void click_exit(object_t *obj, scene_t *scene, window_t *win,
     list_ptr_t *buttons = dico_t_get_value(scene->components, MENU);
     object_t *exit = NULL;
 
-    if (buttons == NULL) {
+    if (buttons == NULL || scene == NULL) {
         return;
     }
     exit = get_element_i_var(buttons, 1);
