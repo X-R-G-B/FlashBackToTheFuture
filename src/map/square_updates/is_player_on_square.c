ab98703fb985d8381a1875e605b3e2b26c6b3705
guillaumeAbel <guillaume.abel@epitech.eu>
2022-04-21 14:28:36 +0200

    Update is_player_on_square

--- src/map/square_updates/is_player_on_square.c
+++ src/map/square_updates/is_player_on_square.c
@@ -7,17 +7,17 @@
 
 #include "my_rpg.h"
 
-static bool check_foot_collision(player_t *player, sfIntRect square_rect)
+static bool check_foot_collision(sfIntRect rect, sfIntRect square_rect)
 {
-    sfIntRect rect = player->obj->bigdata.sprite_bigdata.rect;
-    sfIntRect left_foot = {rect.left + (rect.width * (1 / 10)),
-        rect.top + (rect.height * (4 / 5)), rect.width / 4,
-        rect.height * (2 / 10)};
-    sfIntRect right_foot = {rect.left + rect.width - (rect.width * (1 / 10)),
-        rect.top + (rect.height * (4 / 5)), rect.width / 4,
-        rect.height * (2 / 10)};
+    float height_f = rect.height;
+    int top = (height_f * 0.8) + rect.top;
+    int height = height_f * 0.2;
+    float width_f = rect.width;
+    int left_foot_left = (width_f * 0.1) + rect.left;
+    int right_foot_left = rect.left + rect.width - (width_f * 0.1);
+    sfIntRect left_foot = {left_foot_left, top, rect.width / 4, height};
+    sfIntRect right_foot = {right_foot_left, top, rect.width / 4, height};
 
-    printf("%f %f %f %f\n", rect.height, rect.top, rect.width, rect.)
     if (sfIntRect_intersects(&left_foot, &square_rect, NULL) == sfTrue ||
         sfIntRect_intersects(&right_foot, &square_rect, NULL) == sfTrue) {
         return true;
@@ -28,6 +28,8 @@ static bool check_foot_collision(player_t *player, sfIntRect square_rect)
 bool is_player_on_square(window_t *win, sfIntRect rect)
 {
     player_t *player = NULL;
+    sfFloatRect rect_f = {0};
+    sfIntRect player_rect = {0};
 
     if (win == NULL) {
         return false;
@@ -36,5 +38,8 @@ bool is_player_on_square(window_t *win, sfIntRect rect)
     if (player == NULL) {
         return false;
     }
-    return check_foot_collision(player, rect);
+    rect_f = sfSprite_getGlobalBounds(player->obj->drawable.sprite);
+    player_rect = (sfIntRect) {rect_f.left, rect_f.top, rect_f.width,
+        rect_f.height};
+    return check_foot_collision(player_rect, rect);
 }
