524bd1fe5ebad1b936b3cc0460e176e0d73ac5b3
Romain <romain.panno@epitech.eu>
2022-04-22 15:05:42 +0200

    Fix norm : Break at end of file & printf

--- src/hud/manage_hud.c
+++ src/hud/manage_hud.c
@@ -63,7 +63,6 @@ int add_hud_to_uid_element(scene_t *scene, object_t *hud_data, player_t *player)
         }
     uid_elements = dico_t_get_value(scene->components, UID_ELEMENTS);
     if (uid_elements == NULL) {
-        printf("ok\n");
         return RET_ERR_INPUT;
     }
     uid_apply_right_pos(hud_data, player->obj);
