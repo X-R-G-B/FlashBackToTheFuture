d68bf19032d3602c218fa1a57a48dbad03c916a6
saverio <xavier.mitault@epitech.eu>
2022-04-22 17:01:24 +0200

    Start npc impl

---
+++ src/npc/add_npc.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** myrpg
+** File description:
+** add new non playable character
+*/
+
+#include "my_bgs.h"
+#include "my_rpg.h"
+
+
+
+int add_npc(scene_t *scene, const char *path)
+{
+    if (scene == NULL || path == NULL) {
+        return (RET_ERR_INPUT);
+    }
+    return (RET_OK);
+}
