c6cdfc0de8a7c4c5bdbd211ba520655f6db63a16
saverio <xavier.mitault@epitech.eu>
2022-04-26 21:10:00 +0200

    Fix creation of json bad permissions

--- lib/libjson/src/writer/write_json.c
+++ lib/libjson/src/writer/write_json.c
@@ -9,11 +9,13 @@
 #include <unistd.h>
 #include "my_json.h"
 
+const int modes = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+
 int write_json(any_t *any, char const *path)
 {
     int fd = 0;
 
-    fd = open(path, O_WRONLY | O_TRUNC | O_CREAT);
+    fd = open(path, O_WRONLY | O_TRUNC | O_CREAT, modes);
     if (fd <= 0) {
         return (JS_ERR_INPUT);
     }
