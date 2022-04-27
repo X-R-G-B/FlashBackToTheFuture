84f267e54037b8f822548c03cfcab691ce2d42c6
saverio <xavier.mitault@epitech.eu>
2022-04-27 19:56:54 +0200

    Add countor for magician because the callback is called often

--- src/npc/npc/magician.c
+++ src/npc/npc/magician.c
@@ -15,11 +15,16 @@ void callback_magician(__attribute__((unused)) object_t *npc,
     scene_t *scene,
     __attribute__((unused)) window_t *win)
 {
-    add_text_dialog(scene, "'a' pour passer au prochain dialog", false,
-        NULL);
-    add_text_dialog(scene, "coucou je suis moi", false, NULL);
-    add_text_dialog(scene, "et toi tu es toi", false, NULL);
-    add_text_dialog(scene, "essaie de ne pas te\nfaire tuer par among-us",
-        false, NULL);
-    add_text_dialog(scene, "appuie sur 'l' pour attaquer", false, NULL);
+    static int countor = 0;
+
+    if (countor == 0) {
+        add_text_dialog(scene, "'a' pour passer au prochain dialog", false,
+            NULL);
+        add_text_dialog(scene, "coucou je suis moi", false, NULL);
+        add_text_dialog(scene, "et toi tu es toi", false, NULL);
+        add_text_dialog(scene, "essaie de ne pas te\nfaire tuer par among-us",
+            false, NULL);
+        add_text_dialog(scene, "appuie sur 'l' pour attaquer", false, NULL);
+    }
+    countor++;
 }
