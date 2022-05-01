c0b93eff60386b5d89b35298451c860dd7907d26
saverio <xavier.mitault@epitech.eu>
2022-05-01 23:46:35 +0200

    Add function to rollback when take hurt

--- src/player/update/update_hurt.c
+++ src/player/update/update_hurt.c
@@ -5,22 +5,70 @@
 ** update hurt
 */
 
+#include <SFML/Graphics/View.h>
+#include <SFML/System/Vector2.h>
+#include <stdbool.h>
+#include "ennemies.h"
+#include "my_dico.h"
 #include "my_rpg.h"
 #include "my_json.h"
+#include "math.h"
 
-static void check_blink_time_end(float *since_start, float dtime, any_t *data,
+static const float time_nulll = 1;
+
+const char data_directions[] = "dirrections player hurt";
+
+static void update_move_player(player_t *player, window_t *win, sfVector2f new)
+{
+    ennemy_t enn = {0};
+
+    enn = (ennemy_t) {player->state, player->dir, 1, player->obj, 0, 1};
+    if (check_wall(&enn, new, win) == false) {
+        player->obj->bigdata.sprite_bigdata.pos.x += new.x;
+        player->obj->bigdata.sprite_bigdata.pos.y += new.x;
+        increment_hud_pos(win, get_distance(
+            player->obj->bigdata.sprite_bigdata.pos,
+            sfView_getCenter(player->view)));
+        sfView_setCenter(player->view, player->obj->bigdata.sprite_bigdata.pos);
+    }
+}
+
+static void update_player_rollback(player_t *player, float since_start,
+    window_t *win)
+{
+    sfVector2f *dirrection = NULL;
+    sfVector2f new = {0};
+
+    if (player == NULL || player->obj == NULL) {
+        return;
+    }
+    dirrection = dico_t_get_value(player->obj->components, data_directions);
+    if (dirrection == NULL) {
+        return;
+    }
+    new = (sfVector2f) {dirrection->x / since_start,
+        dirrection->y / since_start};
+    update_move_player(player, win, new);
+}
+
+static void check_blink_time_end(float *since_start, float dtime, window_t *win,
     player_t *player)
 {
-    any_t *blink_time = dico_t_get_any(data->value.dict, "blink time");
+    any_t *data = dico_t_get_value(player->obj->components, PLAYER_DATA);
+    any_t *blink_time = get_from_any(data, "d", "blink time");
 
     if (blink_time == NULL || blink_time->type != FLOAT || player == NULL ||
             player->obj == NULL) {
         return;
     }
     *since_start += dtime;
-    if (*since_start >= blink_time->value.f) {
+    if (*since_start < time_nulll) {
+        update_player_rollback(player, *since_start * (dtime * 1000), win);
+    } else if (*since_start >= blink_time->value.f) {
         *since_start = 0;
         player->obj->components = dico_t_rem(player->obj->components, "hurt");
+        player->obj->components = dico_t_rem(player->obj->components,
+            data_directions);
     }
 }
 
@@ -37,5 +85,5 @@ void update_hurt(player_t *player, __attribute__((unused)) scene_t *scene,
     if (data == NULL) {
         return;
     }
-    check_blink_time_end(&since_start, dtime, data, player);
+    check_blink_time_end(&since_start, dtime, win, player);
 }
