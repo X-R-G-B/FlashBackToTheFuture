707c6b74437642cd3418d9b3b8a67bb444ea5b81
KitetsuK <brice.desousa@epitech.eu>
2022-05-02 15:13:26 +0200

    Modif can now, if you level up, regen you all the energy or life depend on what you upgrade

--- src/player/stat_upgrade/upgrade_stat.c
+++ src/player/stat_upgrade/upgrade_stat.c
@@ -7,9 +7,30 @@
 
 #include "my_rpg.h"
 #include "my_json.h"
+#include "my_strings.h"
 
 extern const char life_max_name[];
 extern const char energy_max_name[];
+
+static void set_stat_max(player_t *player, const char *stat_max_key)
+{
+    any_t *stat = NULL;
+
+    if (player->obj == NULL) {
+        return;
+    }
+    stat = dico_t_get_any(player->obj->components, PLAYER_STATS);
+    stat = get_from_any(stat, "d", stat_max_key);
+    if (stat == NULL) {
+        return;
+    }
+    if (!my_strcmp(stat_max_key, life_max_name)) {
+        player->life = stat->value.f;
+    }
+    if (!my_strcmp(stat_max_key, energy_max_name)) {
+        player->energy = stat->value.f;
+    }
+}
 
 static void upgrade(window_t *win, const char *stat_max_key)
 {
@@ -59,7 +80,7 @@ void upgrade_energy(__attribute__((unused)) object_t *obj,
     if (win == NULL || scene == NULL) {
         return;
     }
-    player = dico_t_get_value(win->components, "player");
+    player = dico_t_get_value(win->components, PLAYER);
     if (player == NULL) {
         return;
     }
@@ -67,6 +88,7 @@ void upgrade_energy(__attribute__((unused)) object_t *obj,
     scene->pause = false;
     if (check_if_pop_up_true(scene->components, STATS_UPGRADE_KEY)) {
         upgrade(win, energy_max_name);
+        set_stat_max(player, energy_max_name);
     }
 }
 
@@ -79,7 +101,7 @@ void upgrade_health(__attribute__((unused)) object_t *obj,
     if (win == NULL || scene == NULL) {
         return;
     }
-    player = dico_t_get_value(win->components, "player");
+    player = dico_t_get_value(win->components, PLAYER);
     if (player == NULL) {
         return;
     }
@@ -87,5 +109,6 @@ void upgrade_health(__attribute__((unused)) object_t *obj,
     scene->pause = false;
     if (check_if_pop_up_true(scene->components, STATS_UPGRADE_KEY)) {
         upgrade(win, life_max_name);
+        set_stat_max(player, life_max_name);
     }
 }
