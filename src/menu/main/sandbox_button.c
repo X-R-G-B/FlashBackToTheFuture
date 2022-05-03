9e916fedd2076f37d267be7a96f51d23a5213bb1
guillaumeAbel <guillaume.abel@epitech.eu>
2022-05-03 08:44:51 +0200

    Add check

--- src/menu/main/sandbox_button.c
+++ src/menu/main/sandbox_button.c
@@ -13,6 +13,9 @@ static const char sandbox_world_path[] =
 void sandbox_button_off(object_t *obj, scene_t *scene, window_t *win,
     set_event_t *evt)
 {
+    if (obj == NULL) {
+        return;
+    }
     launch_stage(win, sandbox_world_path, 0, scene);
     obj->bigdata.sprite_bigdata.rect.left = 0;
 }
