69f16eaa92814f66d9732e95e19152ba609970e0
saverio <xavier.mitault@epitech.eu>
2022-05-07 15:19:49 +0200

    Fix collision array math

--- src/math/rect_contains_segment.c
+++ src/math/rect_contains_segment.c
@@ -11,6 +11,7 @@
 #include <stdbool.h>
 #include "my_bgs.h"
 #include "my_macro.h"
+#include "maths_function.h"
 
 static int nb_intervals = 100;
 
@@ -19,16 +20,15 @@ bool rect_contains_segment(
     sfVector2f point_a,
     sfVector2f point_b)
 {
-    sfVector2f delta_x = {
-        .x = (point_a.x - point_b.x) / nb_intervals,
-        .y = (point_a.x - point_b.y) / nb_intervals
-    };
-    sfVector2f cursor = {.x = point_b.x, .y = point_b.y};
+    sfVector2f delta_x = get_distance(point_a, point_b);
+    sfVector2f cursor = {.x = point_a.x, .y = point_a.y};
 
     if (sfFloatRect_contains(&rect, point_a.x, point_b.y) == sfTrue ||
         sfFloatRect_contains(&rect, point_b.x, point_b.y) == sfTrue) {
         return (true);
     }
+    delta_x.x = delta_x.x / nb_intervals;
+    delta_x.y = delta_x.y / nb_intervals;
     for (int i = 0; i < nb_intervals; i++) {
         if (sfFloatRect_contains(&rect, cursor.x, cursor.y) == sfTrue) {
             return (true);
