891469be98bf33178e798c5b5333c9c9c1513c1f
KitetsuK <brice.desousa@epitech.eu>
2022-05-07 21:38:48 +0200

    Modif name of file for more coherence + Modif functions name for coherence + init music components

--- src/audio/init_sound.c
+++ src/audio/init_sound.c
@@ -7,6 +7,7 @@
 
 #include "my_rpg.h"
 #include "my_bgs_components.h"
+#include "audio.h"
 
 static const char sound_hited_path[] = "./assets/music/sound/roblox.ogg";
 static const char sound_hit_path[] = "./assets/music/sound/hit-marker.ogg";
@@ -20,9 +21,29 @@ static const char sound_open_parchemin[] =
     "./assets/music/sound/paper.ogg";
 static const char sound_level_up[] =
     "./assets/music/sound/level_up_sound.ogg";
+const char MUSIC_GAME[] = "musicgame";
+static const char music_in_game[] =
+    "./assets/music/game_music.ogg";
 static const int layer = 0;
 
-static int init_win_component(window_t *win, scene_t *scene,
+static int init_win_component_music(window_t *win, scene_t *scene,
+    const char *key_to_get, const char *path_to_data)
+{
+    object_t *obj = NULL;
+
+    if (scene == NULL || win == NULL) {
+        return RET_ERR_INPUT;
+    }
+    obj = create_object(NULL, NULL, scene, layer);
+    if (object_set_audio(obj, path_to_data, false, false) != BGS_OK) {
+        return RET_ERR_MALLOC;
+    }
+    add_new_audio(obj, win);
+    window_add_component(win, obj, key_to_get, NULL);
+    return RET_OK;
+}
+
+static int init_win_component_sound(window_t *win, scene_t *scene,
     const char *key_to_get, const char *path_to_data)
 {
     object_t *obj = NULL;
@@ -34,6 +55,7 @@ static int init_win_component(window_t *win, scene_t *scene,
     if (object_set_sound(obj, path_to_data, false, false) != BGS_OK) {
         return RET_ERR_MALLOC;
     }
+    add_new_audio(obj, win);
     window_add_component(win, obj, key_to_get, NULL);
     return RET_OK;
 }
@@ -41,13 +63,14 @@ static int init_win_component(window_t *win, scene_t *scene,
 int init_sounds(scene_t *scene, window_t *win)
 {
 
-    init_win_component(win, scene, HURTED_SOUND, sound_hited_path);
-    init_win_component(win, scene, HURT_SOUNG, sound_hit_path);
-    init_win_component(win, scene, OPEN_INV_SOUND, sound_open_fridge);
-    init_win_component(win, scene, CLOSE_INV_SOUND, sound_close_fridge);
-    init_win_component(win, scene, CONSUM_CAN_SOUND, sound_consum_86);
-    init_win_component(win, scene, INFINITY_86_SOUND, sound_infinity_86);
-    init_win_component(win, scene, PARCHEMIN_SOUND, sound_open_parchemin);
-    init_win_component(win, scene, LEVEL_UP_SOUND, sound_level_up);
+    init_win_component_sound(win, scene, HURTED_SOUND, sound_hited_path);
+    init_win_component_sound(win, scene, HURT_SOUNG, sound_hit_path);
+    init_win_component_sound(win, scene, OPEN_INV_SOUND, sound_open_fridge);
+    init_win_component_sound(win, scene, CLOSE_INV_SOUND, sound_close_fridge);
+    init_win_component_sound(win, scene, CONSUM_CAN_SOUND, sound_consum_86);
+    init_win_component_sound(win, scene, INFINITY_86_SOUND, sound_infinity_86);
+    init_win_component_sound(win, scene, PARCHEMIN_SOUND, sound_open_parchemin);
+    init_win_component_sound(win, scene, LEVEL_UP_SOUND, sound_level_up);
+    init_win_component_music(win, scene, MUSIC_GAME, music_in_game);
     return RET_OK;
 }
