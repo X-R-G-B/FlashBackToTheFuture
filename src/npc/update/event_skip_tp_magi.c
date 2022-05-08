a64e4162ca6145f5774dce163540d0aa91cf980a
saverio <xavier.mitault@epitech.eu>
2022-05-08 23:19:18 +0200

    Remove debug

--- src/npc/update/event_skip_tp_magi.c
+++ src/npc/update/event_skip_tp_magi.c
@@ -22,11 +22,9 @@ void skip_event_magician_tp(object_t *obj,
     any_t *rotation_data = NULL;
     any_t *total_time = NULL;
 
-    printf("ok\n");
     if (obj == NULL) {
         return;
     }
-    printf("oki\n");
     rotation_data = dico_t_get_value(obj->components, rotation_data_key);
     total_time = get_from_any(rotation_data, "d", total_time_key);
     if (total_time == NULL) {
